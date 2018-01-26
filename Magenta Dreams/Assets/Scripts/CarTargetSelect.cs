6a21c55226bfc3777810127a1b31050b800ec7ad
MSI\IfT Lars <egal@test.de>
2018-01-26 20:38:36 +0100

    #8 Assets und Ko

---
+++ Magenta Dreams/Assets/Scripts/CarTargetSelect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class CarTargetSelect : MonoBehaviour {
+
+    AICharacterControl aicc;
+
+	// Use this for initialization
+	void Start () {
+        aicc = GameObject.FindGameObjectWithTag("Auto").GetComponent<AICharacterControl>();	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Haus")
+                {
+                    aicc.Target = hit.transform;
+                }
+                else
+                {
+                    Debug.Log("This isn't a House");
+                }
+            }
+        }
+    }
+}
