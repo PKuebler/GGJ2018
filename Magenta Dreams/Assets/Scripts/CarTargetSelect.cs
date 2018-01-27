96c441015a87c8b80fbb949bb564a2f906c86173
MSI\IfT Lars <egal@test.de>
2018-01-27 16:58:32 +0100

    #1 OnArrived()

--- Magenta Dreams/Assets/Scripts/CarTargetSelect.cs
+++ Magenta Dreams/Assets/Scripts/CarTargetSelect.cs
@@ -15,6 +15,7 @@ public class CarTargetSelect : MonoBehaviour {
 
     public bool Working { get; private set; }
 
+    private float distance;
 
     void Start ()
     {
@@ -25,6 +26,18 @@ public class CarTargetSelect : MonoBehaviour {
         agent = GetComponent<NavMeshAgent>();
 
         Working = false;
+    }
+
+    void Update()
+    {
+        if (GetComponent<AICharacterControl>().Target != null)
+        {
+            distance = Vector3.Distance(this.transform.position, GetComponent<AICharacterControl>().Target.transform.position);
+            if (distance < 2.8f)
+            {
+                GetComponent<AICharacterControl>().Target.GetComponent<Building>().OnArrived();
+            }
+        }
     }
 
 	public void ReachedTarget(GameObject triggerObj, bool isWorking)
