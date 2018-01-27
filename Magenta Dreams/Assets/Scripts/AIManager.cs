ca703e4867ba56a9635ccbfb551adcb28c9ba3e0
MSI\IfT Lars <egal@test.de>
2018-01-27 11:39:42 +0100

    # Häuser haben Owner Anfang KI

--- Magenta Dreams/Assets/Scripts/AIManager.cs
+++ Magenta Dreams/Assets/Scripts/AIManager.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class AIManager : MonoBehaviour {
 
+    private GameObject[] carArray;
+    private List<GameObject> carList;
+    private GameObject[] buildingArray;
+    public List<GameObject> BuildingsWithEvents { get; set; }
+
 	// Use this for initialization
 	void Start () {
-		
+        carArray = GameObject.FindGameObjectsWithTag("KIAuto");
+        foreach (GameObject go in carArray)
+        {
+            carList.Add(go);
+        }
+        buildingArray = GameObject.FindGameObjectsWithTag("Haus");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+
+    public void AddEvent (GameObject building)
+    {
+
+    }
+
+    public void RemoveEvent (GameObject building)
+    {
+
+    }
 }
