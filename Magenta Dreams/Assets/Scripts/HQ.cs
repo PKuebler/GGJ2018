cc40ab081175c4240619835595469c28e807bc26
Adrian Seehawer <a.seehawer@me.com>
2018-01-27 01:13:18 +0100

    #5 HQ - Anzahl Cars wird nun angezeigt - Refactor: * Init() Methode erstellt * Startet mit einem Auto (+ im Money für erstes Auto) * Gebäude Array und Liste erstellt

--- Magenta Dreams/Assets/Scripts/HQ.cs
+++ Magenta Dreams/Assets/Scripts/HQ.cs
@@ -2,29 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HQ : MonoBehaviour {
+public class HQ : MonoBehaviour 
+{
 
 	public GameObject carPrefab;
+	public GameObject buildingPrefab;
 	private int money;
 	private int carPrice;
-	private GameObject[] buildingList;
+	private GameObject[] buildingArray;
 	private GameObject[] carArray;
 	private List<GameObject> carList;
+	private List<GameObject> buildingList;
+	private Quaternion rotation;
 
 	// Use this for initialization
 	void Start () {
-		money = 1000;
-		carPrice = 100;
-		// buildingList = GameObject.FindGameObjectsWithTag ("Haus");
 
-		carList = new List<GameObject>();
-
-		carArray = GameObject.FindGameObjectsWithTag("Auto"); 
-
-		for (int i = 0; i < carArray.Length; i++) 
-		{
-			carList.Add(carArray[i]);
-		}
+		InitStats ();
 
 	}
 	
@@ -41,9 +35,7 @@ public class HQ : MonoBehaviour {
 		{
 			for(int i = 0; i < numbersOfCars; i++ )
 			{
-
-				Vector3 postition = new Vector3 (this.transform.position.x + (i + 1.5f), this.transform.position.y, this.transform.position.z);
-				Quaternion rotation = new Quaternion (0, 0, 0, 0);
+				Vector3 postition = new Vector3 (this.transform.position.x + (i + 1.0f), this.transform.position.y, this.transform.position.z);
 				GameObject newCar = Instantiate (carPrefab, postition, rotation, this.transform);
 				carList.Add (newCar);
 				money = money - carPrice;
@@ -52,11 +44,43 @@ public class HQ : MonoBehaviour {
 		else
 		{
 			// Throw Masage -> insufficient funds
+			// DisplayDialog ("Title here", "Your text", "Ok");
 		}
 	}
 
 	void OnGUI ()
 	{
-		GUI.Label (new Rect (0, 100, 100, 50), money.ToString ());
+		GUI.Label (new Rect (0, 100, 200, 50), "Funds: " + money.ToString () + "\nCars: " + carList.Count.ToString() );
 	}
+
+	#region Erstellen der HQ Werte und erstes Auto
+	void InitStats()
+	{
+		// HQ Stats
+		money = 1100;
+		carPrice = 100;
+
+		rotation = new Quaternion (0, 0, 0, 0);
+
+		// Setzen der Gebäude die bei Spielstart zum HQ gehören
+		buildingList = new List<GameObject> ();
+		buildingArray = GameObject.FindGameObjectsWithTag ("Haus");
+
+		for (int i = 0; i < buildingArray.Length; i++) 
+		{
+			buildingList.Add(buildingArray[i]);
+		}
+
+		// Setzen der Autos die bei Spielstart zum HQ gehören
+		carList = new List<GameObject>();
+		carArray = GameObject.FindGameObjectsWithTag("Auto"); 
+
+		for (int i = 0; i < carArray.Length; i++) 
+		{
+			carList.Add(carArray[i]);
+		}
+		// Kauf des ersten Autos
+		BuyCars ();
+	}
+	#endregion
 }
