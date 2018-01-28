53dea5f56997810fd2ba87dcb3a6e010a0d7f810
Philipp Kübler <philipp@sourceboat.com>
2018-01-28 13:59:09 +0100

    color buildings, prefab fix

---
+++ Magenta Dreams/Assets/Scripts/MaterialStorage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialStorage : MonoBehaviour {
+
+	public Material aiMaterial;
+	public Material playerMaterial;
+	public Material standardMaterial;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
