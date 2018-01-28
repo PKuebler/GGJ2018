548db91247c478b6650c587ef22dc7c3b445557e
MSI\IfT Lars <egal@test.de>
2018-01-28 12:49:50 +0100

    #8 WinLoseScript

---
+++ Magenta Dreams/Assets/Scripts/WinLose.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLose : MonoBehaviour {
+
+    private bool endAfterTimer;
+    public bool EndAfterTimer { get; set; }
+    private GameObject[] buildings;
+
+    private int playerHouses;
+    public int PlayerHouses
+    { get { return playerHouses; } }
+
+    private int aiHouses;
+    public int AIHouses
+    { get { return aiHouses; } }
+
+    // Use this for initialization
+    void Start () {
+        buildings = GameObject.FindGameObjectsWithTag("Haus");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateHouseCount();
+	}
+
+    private void UpdateHouseCount()
+    {
+        playerHouses = 0;
+        aiHouses = 0;
+
+        foreach (GameObject house in buildings)
+        {
+            if (house.GetComponent<Building>().owner == Building.Owner.Player)
+                playerHouses++;
+            if (house.GetComponent<Building>().owner == Building.Owner.AI)
+                aiHouses++;
+        }
+    }
+}
